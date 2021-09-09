45cd1aa8c9a2f0ed6083cc655e8eb97e4a0d80ae
theminecr123 <81743072+theminecr123@users.noreply.github.com>
2021-09-09 20:21:14 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+ int n;
+ printf("Nhap vao so n: "); scanf("%d",&n);
+ for(int i=1;i<=2*n-1;i++)
+ 	{
+ 		printf("%5d",i);
+ 		
+ 	}
+ getch();
+ return 0;
+}
